e07df31748aac05f049fc6f4cf89c6f43487e8b1
Michael Wilson <mw0415436@otc.edu>
2019-09-15 21:28:00 -0500

    Migrated some databases

---
+++ database/migrations/2019_09_16_021648_create_projects_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+   
+    public function down()
+    {
+       // Schema::dropIfExists('projects');
+    }
+}
+
+
